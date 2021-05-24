e013f18182b0f1c42f57fa144c5d498946cc0170
Jonas Fredriksson <48363473+Miercolez@users.noreply.github.com>
2021-05-24 14:45:59 +0200

    Added server and client example

--- core/src/main/java/Main.java
+++ core/src/main/java/Main.java
@@ -1,10 +1,28 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
 
     public static void main(String[] args) {
+        //localhost 127.0.0.1
+        // Martins ip: 178.174.162.51
 
-        var utils = new Utils();
+        try(ServerSocket socket = new ServerSocket(80)){
 
-        System.out.println(utils.message());
+            while (true) {
+                Socket client = socket.accept();
+                System.out.println(client.getInetAddress());
+                var inputFromClient = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+                inputFromClient.lines().forEach(System.out::println);
+                inputFromClient.close();
+                client.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
